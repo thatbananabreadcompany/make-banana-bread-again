2e976c9e9b5581120fe6ece5471f297f6b390f80
TBBC <thatbananabreadcompany@gmail.com>
2026-06-10 11:29:54 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { inject } from '@vercel/analytics'
 import App from './App.jsx'
+
+inject()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
